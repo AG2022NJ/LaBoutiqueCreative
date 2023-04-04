c1eb886652de438728c0a7fe29c59439bc3cf222
alexandrine GUILLOT <sandy.guillot@hotmail.fr>
2023-04-04 09:53:30 +0200

    submit

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,9 +5,14 @@ namespace App\Form;
 use App\Entity\User;
 use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButtonTypeInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Form\Core\Type;
+
 
 
 
@@ -21,6 +26,9 @@ class RegisterType extends AbstractType
             ->add('lastname')
             ->add('email')
             ->add('password')
+            ->add( child: 'submit',type: SubmitType::class, options: [
+                'label' => 'Inscription'
+            ] )
         ;
     }
 
